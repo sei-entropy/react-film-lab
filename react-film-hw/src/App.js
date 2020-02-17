4ca63c6164b8f29a45ff5d0367f8d5ad2008a614
vex-xcc <vex.xcc@gmail.com>
2020-02-17 19:23:27 +0300

    part1 done !

---
+++ react-film-hw/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import FilmDetails from './FilmDetails';
+import FilmListing from './FilmListing';
+import TMDB from './TMDB';
+
+function App() {
+  return (
+    <div className="film-library">
+    <FilmListing movies={TMDB}/>
+    <FilmDetails movies={TMDB}/>
+   </div>
+);}
+
+export default App
