76d68c478ce5fed9c4439978f0fc893a23f2a24b
bader_mazro <bader.mazro@gmail.com>
2020-02-17 22:03:18 +0300

    All part One done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import FilmListing from './FilmListing';
-import FilmDetails from './FilmDetails'
+import FilmDetails from './FilmDetails';
+import TMDB from './TMDB';
+
+
 
 export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log(TMDB.films);
+  
+  }
+ 
   render(){
+   
+  
+
     return (<div className="film-library">
     <div>
       <FilmListing/>
     </div>
+    
     <div>
       <FilmDetails/>
     </div>
--- src/FilmListing.js
+++ src/FilmListing.js
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import TMDB from './TMDB';
 
 export default class FilmListing extends React.Component{
     render(){
+        const alldata = TMDB.films.map(function(data,index){
+            const item = "https://image.tmdb.org/t/p/w1280"+ data.poster_path
+            return <div className="film-row"><img src={item} alt="" />
+             <div className="film-summary">
+            <h1>{data.title}</h1>
+            <p>{data.release_date}</p>
+            </div>
+            </div>
+            
+            //https://image.tmdb.org/t/p/w1280{data.poster_path}
+            })
         return (<div className="film-list">
         <h1 className="section-title">FILMS</h1>
-        </div>)
+        {alldata }
+        </div>
+
+        )
     }
 }
