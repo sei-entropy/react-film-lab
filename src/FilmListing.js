fab4749905785587165fa8a7a35fb1e5429f86bd
robaarishi24 <robaarishi@hotmail.com>
2020-02-17 20:49:17 +0300

    Part-1 done

---
+++ src/FilmListing.js
@@ -0,0 +1,17 @@
+import React from "react";
+import FilmRow from "./FilmRow";
+
+export default class FilmListing extends React.Component{
+  render(){
+    const allFilms = this.props.movies.films.map((film, index) => {
+        return <FilmRow film={film} key={index}/>
+    })
+
+    return(
+        <div className="film-list">
+            <h1 className="section-title">FILMS</h1>
+            {allFilms}
+        </div>
+    );
+  }
+}
