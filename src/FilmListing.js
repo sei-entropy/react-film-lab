9a10444b1ccf4017db62527fb11f59c6a44eaf48
Rawan991 <rawan.alrajeh@gmail.com>
2020-02-17 23:14:38 +0300

    List

---
+++ src/FilmListing.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import TMDB from './TMDB'
+import FilmRow from './FilmRow'
+export default class FilmListing extends React.Component {
+    render() {
+
+        const allFilms = this.props.list.map( (film, index) => {
+            return <FilmRow key={film.id} film={film} />
+
+        });
+
+        return (
+
+       <div> {allFilms} </div> 
+        )
+        
+    }
+
+}
