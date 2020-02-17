cebc865af34468e8809fc6819ce5450865e9eb01
bader_mazro <bader.mazro@gmail.com>
2020-02-17 20:03:00 +0300

    doing the first requirment

---
+++ src/FilmListing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default class FilmListing extends React.Component{
+    render(){
+        return (<div className="film-list">
+        <h1 className="section-title">FILMS</h1>
+        </div>)
+    }
+}
