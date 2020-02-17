ff56a00d1ac86647633cf51401dfdc7566c3d282
bader_mazro <bader.mazro@gmail.com>
2020-02-17 22:55:47 +0300

    adding file

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import TMDB from './TMDB';
+import FilmPoster from './FilmPoster'
+
 
 export default class FilmListing extends React.Component{
     render(){
-        const alldata = TMDB.films.map(function(data,index){
-            const item = "https://image.tmdb.org/t/p/w1280"+ data.poster_path
-            return <div className="film-row"><img src={item} alt="" />
-             <div className="film-summary">
-            <h1>{data.title}</h1>
-            <p>{data.release_date}</p>
-            </div>
-            </div>
-            
-            //https://image.tmdb.org/t/p/w1280{data.poster_path}
-            })
+     
         return (<div className="film-list">
         <h1 className="section-title">FILMS</h1>
-        {alldata }
+        <FilmPoster/>
         </div>
 
         )
