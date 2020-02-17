549bd53f0e9b9fc211f561e0b43740ec846780c8
Rawan991 <rawan.alrajeh@gmail.com>
2020-02-17 23:12:34 +0300

    Row

---
+++ src/FilmRow.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import TMDB from './TMDB'
+import FilmPoster from './FilmPoster'
+
+export default class FilmRow extends React.Component {
+    render() {
+        const years = new Date(this.props.film.release_date)
+
+        return (
+            <div className="film-row">
+                ${this.props.posterPath}
+           <FilmPoster posterPath={this.props.film.poster_path} />
+
+                <div className="film-summary">
+                    <h1>{this.props.film.title}</h1>
+                    <p>{years.getFullYear()}</p>
+                </div>
+            </div>
+
+        )
+    }
+}
