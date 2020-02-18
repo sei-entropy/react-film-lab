67107fb80ff8330df2e448a24991096958604658
robaarishi24 <robaarishi@hotmail.com>
2020-02-19 01:26:57 +0300

    part2 done

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -2,6 +2,19 @@ import React from "react";
 import FilmRow from "./FilmRow";
 
 export default class FilmListing extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+    filter: 'all'
+  }
+}
+  handleFilterClick = (filter)=> {
+    console.log('Setting filter to',filter)
+    this.setState({
+      filter: filter
+    })
+  }
   render(){
     const allFilms = this.props.movie.films.map((film, index) => {
         return <FilmRow 
@@ -10,10 +23,26 @@ export default class FilmListing extends React.Component{
     })
 
     return(
-        <div className="film-list">
-        <h1 className="section-title">FILMS</h1>
-            {allFilms}
-        </div>
-    );
+      <div className="film-list">
+      <h1 className="section-title">FILMS</h1>
+
+      <div className="film-list-filters">
+      <div onClick={() => this.handleFilterClick('all')}  
+             className={`film-list-filter ${this.state.filter === 'all' ? 'is-active' : ''}`}>
+          ALL
+          <span className="section-count">{this.props.movie.films.length}</span>
+          </div>
+                    
+      <div onClick={() => this.handleFilterClick('faves')} 
+      className={`film-list-filter ${this.state.filter === 'faves' ? 'is-active' : ''}`}>
+          FAVES
+      <span className="section-count">0</span>
+      </div>
+      </div>
+
+    {allFilms}
+
+    </div>
+      );
   }
-}
+} 
--- src/FilmRow.js
+++ src/FilmRow.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import FilmPoster from './FilmPoster'
+import Fave from './Fave'
+
 
 export default class FilmRow extends React.Component {
+    handleFilterClick = (film) => {
+        console.log("Fetching details for: ", film);
+    }
     render(){
         const movieYears = new Date(this.props.film.release_date);
 
         return(
 
-            <div className="film-row">
-                <FilmPoster 
-                film ={this.props.film}/>
+            <div className="film-row" onClick={ () => this.handleFilterClick(this.props.film.title)}>
 
+                <FilmPoster film={this.props.film}/>
+            
                 <div className="film-summary">
+                <Fave/>
                 <h1>{this.props.film.title}</h1>
                 <p>{ movieYears.getFullYear()}</p>
                 </div>
