8cb169ae29a62ecbea38ea7cfb29bcb01500c951
Sager Alarifi <sagr.1993@gmail.com>
2020-02-18 14:24:24 +0300

    building button

--- src/Fave.js
+++ src/Fave.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 
-export default class Fave extends Component {
+class Fave extends Component {
+  handleClick = e => {
+    e.stopPropagation();
+    console.log("not working");
+  };
   render() {
     return (
-      <div className="film-row-fave add_to_queue">
+      <div className="film-row-fave add_to_queue" onClick={this.handleClick}>
         <p className="material-icons">add_to_queue</p>
       </div>
     );
   }
 }
+export default Fave;
