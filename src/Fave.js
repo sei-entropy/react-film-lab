1cad99383c3572316b67993434636975a897b84d
Sager Alarifi <sagr.1993@gmail.com>
2020-02-18 13:24:52 +0300

    create Fave Components

---
+++ src/Fave.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Fave extends Component {
+  render() {
+    return (
+      <div className="film-row-fave add_to_queue">
+        <p className="material-icons">add_to_queue</p>
+      </div>
+    );
+  }
+}
