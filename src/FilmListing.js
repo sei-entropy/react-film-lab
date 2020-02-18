25f3880945d5ad765033a667715888eef4084373
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-02-18 20:37:34 +0300

    Add Part 2

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -2,6 +2,20 @@ import React from "react";
 import FilmRow from "./FilmRow";
 
 class FilmListing extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            filter: "all"
+        };
+    }
+
+    handleFilterClick = filter => {
+        this.setState({
+            filter: filter
+        });
+    };
+
     render() {
         const allFilms = this.props.films.map(film => {
             return <FilmRow key={film.id} film={film} />;
@@ -10,6 +24,29 @@ class FilmListing extends React.Component {
         return (
             <div className="film-list">
                 <h1 className="section-title">FILMS</h1>
+                <div className="film-list-filters">
+                    <div
+                        className={`film-list-filter ${
+                            this.state.filter === "all" ? "is-active" : ""
+                        }`}
+                        onClick={() => this.handleFilterClick("all")}
+                    >
+                        ALL
+                        <span className="section-count">
+                            {this.props.films.length}
+                        </span>
+                    </div>
+                    <div
+                        className={`film-list-filter ${
+                            this.state.filter === "faves" ? "is-active" : ""
+                        }`}
+                        onClick={() => this.handleFilterClick("faves")}
+                    >
+                        FAVES
+                        <span className="section-count">0</span>
+                    </div>
+                </div>
+
                 {allFilms}
             </div>
         );
