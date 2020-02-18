4353ffb3c0b63bc3ab41df328ca7a9293c676c87
Sager Alarifi <sagr.1993@gmail.com>
2020-02-18 11:17:47 +0300

    change files to jsx, create and build FilmPoster, FilmRow, FilmList

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import FilmList from "FilmList.jsx";
-import FilmDetails from "FilmDetails.jsx";
+import FilmList from "./FilmList.jsx";
+import FilmDetails from "./FilmDetails.jsx";
+import TMDB from "./TMDB.jsx";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="film-library">
-      <FilmList />
-      <FilmDetails />
+      <FilmList films={TMDB.films} />
+      <FilmDetails films={TMDB.films} />
     </div>
   );
 }
---
+++ src/FilmPoster.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class FilmPoster extends Component {
+  render() {
+    return (
+      <div>
+        <img src={this.props.posterUrl} alt={this.props.title} />
+      </div>
+    );
+  }
+}
