f01c771fd183af154da41ab04006d0bc48a9932c
Afnan MT <afnanxmt@gmail.com>
2020-02-19 02:00:55 +0300

    part 2 Done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TMDB from './TMDB.js'
 import FilmListing from './FilmListing.js';
 import FilmDetails from './FilmDetails.js';
+
 import './App.css';
 
 export default class App extends React.Component { 
@@ -12,6 +13,7 @@ export default class App extends React.Component {
     return <div className='film-library'>   
       < FilmListing films={ TMDB.films } />
     <FilmDetails films={ TMDB.films } />
+   
     </div>
   }
 }
--- src/FilmListing.js
+++ src/FilmListing.js
@@ -4,6 +4,24 @@ import FilmRow from './FilmRow.js'
 
 
 export default class FilmListing extends React.Component { 
+
+  constructor(props){ 
+    super (props); 
+
+    this.state = { 
+      filter: 'all',
+    }
+
+  }
+  handleFilterClick = (filter ) => { 
+console.log('Just filter',filter)
+      	this.setState({
+         filter: filter
+          });
+  }
+  
+  
+  
   render() 
   { 
         const allFilms = this.props.films.map(function (film){
@@ -17,6 +35,16 @@ export default class FilmListing extends React.Component {
 
     return    <div className="film-list">
     <h1 className="section-title">FILMS</h1>
+    <div className="film-list-filters">
+        <div className={`film-list-filter ${this.state.filter === 'all' ? 'is-active' : ''}`}onClick={() => this.handleFilterClick('all')}  >
+            ALL
+            <span className="section-count">{this.props.films.length}</span>
+        </div>
+        <div className={`film-list-filter ${this.state.filter === 'faves' ? 'is-active' : ''}`} onClick={() => this.handleFilterClick('faves')} >
+            FAVES
+            <span className="section-count">0</span>
+        </div>
+    </div>
     {allFilms}
     
   </div>
