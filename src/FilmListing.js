ac247fc7d71aad3e3d7a5b4654b87fc35119869f
Rawan991 <rawan.alrajeh@gmail.com>
2020-02-18 08:58:50 +0300

    List file

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -4,16 +4,16 @@ import FilmRow from './FilmRow'
 export default class FilmListing extends React.Component {
     render() {
 
-        const allFilms = this.props.list.map( (film, index) => {
+        const allFilms = this.props.list.map((film, index) => {
             return <FilmRow key={film.id} film={film} />
 
         });
 
         return (
 
-       <div> {allFilms} </div> 
+            <div> {allFilms} </div>
         )
-        
+
     }
 
 }
