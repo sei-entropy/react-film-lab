1f15fcbedab6dcb619c802e9f31e1329eb53fd24
Najwab <inajjwa2@gmail.com>
2020-02-21 14:40:45 +0300

    Part Four

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import FilmListing from './FilmListing';
 import FilmDetails from './FilmDetails';
 import TMDB from './TMDB';
+import axios from "axios";
 
 export default class App extends React.Component{
     constructor(props){
@@ -42,12 +43,26 @@ export default class App extends React.Component{
            })
 
     }
-    handleDetailsClick=(e)=>{
-        console.log("Fetching details for :"+e.title);
+    handleDetailsClick=(film)=>{
+        const api_key="f4531f33ce4dc189cb7e485fda3f54ac";
+        const url = `https://api.themoviedb.org/3/movie/${film.id}?api_key=${api_key}&append_to_response=videos,images&language=en`;
+                
+        axios({
+        method: 'GET',
+        url: url
+        }).then(response => {
+        console.log(response) ;
+        console.log("Fetching details for :"+film.title);
+        this.setState({
+            current:response.data
+          })
 
-        // this.setState({
-        //     current:film
-        //   })
+        })
+        .catch(err => {
+            console.log('ERROR: ', err);
+        });
+
+
         
     }
 
--- src/FilmDetails.js
+++ src/FilmDetails.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
 export default class FilmDetails extends React.Component{
+    
 
     render(){
+
+        const backdropUrl = `https://image.tmdb.org/t/p/w1280/${this.props.film.backdrop_path}`;
+        const posterUrl = `https://image.tmdb.org/t/p/w780/${this.props.film.poster_path}`;
+        let details
+            
+            if(this.props.film.id === undefined){
+                details= <div className="film-detail"><p><i className="material-icons">subscriptions</i><span>No film selected</span></p></div>;
+            }else{
+         details=<div className="film-detail is-hydrated">
+            <figure className="film-backdrop">
+                <img src={backdropUrl} alt="" />
+                <h1 className="film-title">{this.props.film.title}</h1>
+            </figure>
+
+            <div className="film-meta">
+                <h2 className="film-tagline">{this.props.film.tagline}</h2>
+                <p className="film-detail-overview">
+                <img src={posterUrl} className="film-detail-poster" alt={this.props.film.title} />
+                {this.props.film.overview}
+                </p>
+            </div>
+        </div>;
+            }
         return (
           
      <div className="film-details">
         <h1 className="section-title">{this.props.detailsName}</h1>
+        {details}
      </div>
 
         );
